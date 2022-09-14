2df5ff1192cc331db805c5fe41a91aa39aee7fff
Taryn Hovenkamp <taho3676@colorado.edu>
2022-09-14 01:18:52 -0600

    Initial Setup
    Committed initial app setup

---
+++ SchedulingApp/SchedulingApp/SchedulingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SchedulingAppApp.swift
+//  SchedulingApp
+//
+//  Created by CUBS Customer on 9/14/22.
+//
+
+import SwiftUI
+
+@main
+struct SchedulingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
